8ad49008aa9bce27d82d5a13c8bbd7f0bd25b22c
Phaneendra Kumar <87582519+phani051@users.noreply.github.com>
2025-07-24 18:48:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 from dotenv import load_dotenv
 import os
-BOT_TOKEN = os.getenv("BOT_TOKEN"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 DB_NAME = "expenses.db"
